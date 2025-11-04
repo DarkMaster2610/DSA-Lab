795b0517ff38b96e9322a060bc708c37fe45174c
DarkMaster2610 <11249a016@kanchiuniv.ac.in>
2025-11-04 12:00:07 +0530

    Ex 9d: merge sort

--- main.c
+++ main.c
@@ -1,40 +1,53 @@
 #include <stdio.h>
-void swap(int* a, int* b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; // pivot
-    int i = low - 1;  // Index of smaller element
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    // Create temp arrays
+    int L[n1], R[n2];
+    // Copy data
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    i = 0; j = 0; k = l;
+    // Merge temp arrays back into arr[l..r]
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    return i + 1;
+    // Copy remaining elements of L[], if any
+    while (i < n1) {
+        arr[k++] = L[i++];
+    }
+    // Copy remaining elements of R[], if any
+    while (j < n2) {
+        arr[k++] = R[j++];
+    }
 }
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
 }
 void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++)
+    for (int i=0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5};
+    int arr[] = {12, 11, 13, 5, 6, 7};
     int n = sizeof(arr)/sizeof(arr[0]);
     printf("Original array: ");
     printArray(arr, n);
-    quickSort(arr, 0, n - 1);
-    printf("Sorted array (Quick Sort): ");
+    mergeSort(arr, 0, n - 1);
+    printf("Sorted array (Merge Sort): ");
     printArray(arr, n);
     return 0;
 }
